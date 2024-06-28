ef1cae3f90b25ffc6c0609156424f441a5d0c15d
Meiyi Zheng <meiyizheng@yahoo.com>
2024-06-28 13:42:22 -0700

    add a return type

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,7 +31,10 @@ export type State = {
   message?: string | null;
 };
 
-export async function createInvoice(prevState: State, formData: FormData) {
+export async function createInvoice(
+  prevState: State,
+  formData: FormData
+): Promise<State> {
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -67,8 +70,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
 export async function updateInvoice(
   id: string,
   prevState: State,
-  formData: FormData,
-) {
+  formData: FormData
+): Promise<State> {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
